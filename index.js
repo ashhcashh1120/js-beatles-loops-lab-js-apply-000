649253e85c80ab958a44caf094779c5c5a27ab39
Ashley Achibald <aarchibald20@gmail.com>
2018-12-20 16:07:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function johnLennonFacts(facts){
   }return results;
 }
 // add solution here
+function iLoveTheBeatles(num){
+  var array = []; 
+  do{
+    array.push("I love the Beatles!")
+    num++;
+  }
+  while(num < 15);
+}return array;
+
