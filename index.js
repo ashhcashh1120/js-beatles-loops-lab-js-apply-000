283dc3831bab9010859fd102bc68d2582e308a51
Ashley Achibald <aarchibald20@gmail.com>
2018-12-20 15:01:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
   var arr = [];
-  for(let i = 0 ; i < musicians.length; i++){
+  for(let i = 0 ; i < arr.length; i++){
     arr.push(musicians[i] + "plays" + instruments[i])
   }
 }
