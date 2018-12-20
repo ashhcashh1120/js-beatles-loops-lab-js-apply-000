bf0e9d78c7c52749471d64074f05985e175ca12e
Ashley Achibald <aarchibald20@gmail.com>
2018-12-20 16:07:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function johnLennonFacts(facts){
 }
 // add solution here
 function iLoveTheBeatles(num){
-  var array = []; 
+  var array = [];
   do{
     array.push("I love the Beatles!")
     num++;
   }
   while(num < 15);
 }return array;
-
+}
